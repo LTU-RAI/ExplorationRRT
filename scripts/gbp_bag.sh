58455fce66ed73da8d0eed9c03e858be2b3b5b0e
aakapatel <patelakash94293@gmail.com>
2024-07-07 00:04:06 +0200

    removed unnecessary files

--- scripts/gbp_bag.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-read bag_name
-
-rosbag record -O $bag_name /tf /tf_static /rmf_obelix/ground_truth/odometry /errt_execution_time /command_path /octomap_point_cloud_centers /vis/ref_path /rmf_obelix/velodyne_points     
--- scripts/rosbag.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-read bag_name
-
-rosbag record -o $bag_name /tf /tf_static /ufomap_mapping_server_node/map_depth_1 /tracked_trajectory /hummingbird/velodyne_points /hummingbird/reference /hummingbird/ground_truth/odometry /RRT_PATHS /RRT_NODES /RRT_GOALS /RESERVED_GOALS /POSITION /PATH_TAKEN /Internal_ufo_map /HITS /CHOSEN_RRT_PATH_VISUALIZATION /CHOSEN_RRT_PATH_VISUALIZATION_array /chosen_path /errt_execution_time /command_path /hummingbird/command/roll_pitch_yawrate_thrust /hummingbird/command/pose /octomap_point_cloud_centers 
-
- 
